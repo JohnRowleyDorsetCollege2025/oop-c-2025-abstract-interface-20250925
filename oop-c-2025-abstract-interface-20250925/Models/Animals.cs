add803de1bde1e1d762f297afb4212130ea8da21
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 18:02:53 +0100

    added interface

--- oop-c-2025-abstract-interface-20250925/Models/Animals.cs
+++ oop-c-2025-abstract-interface-20250925/Models/Animals.cs
@@ -6,26 +6,91 @@ using System.Threading.Tasks;
 
 namespace oop_c_2025_abstract_interface_20250925.Models
 {
+
+    public interface IFlyable
+    {
+        public void Fly();
+        public void Land();
+    }
+
+
     internal abstract class Animal
     {
+        // constructor to initialize name
+        protected Animal(string name)
+        {
+           Name = name;
+        }
         public string Name { get; set; } = string.Empty;
+
+        public virtual void Describe() =>
+            Console.WriteLine($"This is an animal named {Name}.");
 
         public abstract void Speak();
     }
 
     class Dog : Animal
     {
+        // constructor to initialize name
+        public Dog(string name) : base(name)
+        {
+            
+        }
         public override void Speak()
         {
-            ///throw new NotImplementedException();
+            Console.WriteLine("Woof!"); 
         }
+    }
+
+    class Wolf : Dog
+    {
+        // constructor to initialize name
+        public Wolf(string name) : base(name)
+        {
+            
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("Howl!");
+        }
+        public override void Describe() =>
+            Console.WriteLine($"This is a wild animal named {Name}.");
     }
 
     class Cat : Animal
     {
+        public Cat(string name) : base(name)
+        {
+            
+        }
         public override void Speak()
         {
-          ///  throw new NotImplementedException();
+          Console.WriteLine("Meow!");
+        }
+    }
+
+    class Bird : Animal, IFlyable
+    {
+        
+        
+    
+        public Bird(string name) : base(name)
+        {
+
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} is flying.");
+        }
+        public void Land()
+        {
+            Console.WriteLine($"{Name} has landed.");
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("chirp chirp");
         }
     }
 }
--- oop-c-2025-abstract-interface-20250925/Program.cs
+++ oop-c-2025-abstract-interface-20250925/Program.cs
@@ -3,14 +3,27 @@ using oop_c_2025_abstract_interface_20250925.Models;
 
 Console.WriteLine("Hello, World!");
 
-//Animal animal = new Animal();
+// Animal animal = new Animal();
 
-Dog dog = new Dog();
-dog.Name = "Fido";
-Cat cat = new Cat();
-cat.Name = "Whiskers";
+Dog dog = new Dog("Fido");
+Cat cat = new Cat("Whiskers");
+Wolf wolf = new Wolf("Ghost");
+Bird bird = new Bird("Birdie");
+
+
 Console.WriteLine($"Dog's name: {dog.Name}");
 Console.WriteLine($"Cat's name: {cat.Name}");
 
 cat.Speak();
 dog.Speak();
+wolf.Speak();
+bird.Speak();
+
+
+dog.Describe();
+cat.Describe();
+wolf.Describe();
+bird.Describe();
+
+bird.Fly();
+bird.Land();
