ceb6ffbce5388bbfb9de7bad05a67c5c9cc03c65
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 16:17:30 +0100

    Add project files.

---
+++ oop-c-2025-abstract-interface-20250925/Models/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_c_2025_abstract_interface_20250925.Models
+{
+    internal abstract class Animal
+    {
+    }
+
+    class Dog : Animal
+    {
+
+    }
+
+    class Cat : Animal
+    {
+    }
+}
---
+++ oop-c-2025-abstract-interface-20250925/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using oop_c_2025_abstract_interface_20250925.Models;
+
+Console.WriteLine("Hello, World!");
+
+//Animal animal = new Animal();
+
+Dog dog = new Dog();
+Cat cat = new Cat();
