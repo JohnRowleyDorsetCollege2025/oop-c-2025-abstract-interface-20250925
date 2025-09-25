a978d7071c6a5a431ec966b709afb38d1e56c625
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 16:44:18 +0100

    dd

--- oop-c-2025-abstract-interface-20250925/Models/Animals.cs
+++ oop-c-2025-abstract-interface-20250925/Models/Animals.cs
@@ -8,7 +8,7 @@ namespace oop_c_2025_abstract_interface_20250925.Models
 {
     internal abstract class Animal
     {
-        public string Name { get; set; }    = string.Empty;
+        public string Name { get; set; } = string.Empty;
 
         public abstract void Speak();
     }
